24243030afaa87d1e548550f7daf1396a6dd10db
Rex Zhang <rexzhang4321@gmail.com>
2022-07-14 18:36:29 -0700

    add more editor key bindings

--- src/main/java/components/KeyControls.java
+++ src/main/java/components/KeyControls.java
@@ -12,15 +12,21 @@ import java.util.List;
 
 public class KeyControls extends Component {
 
+    private float debounceTime = 0.2f;
+    private float debounce = 0f;
+
     @Override
     public void editorUpdate(float dt) {
+        debounce -= dt;
+        float multiplier = KeyListener.isKeyPressed(GLFW.GLFW_KEY_LEFT_SHIFT) ? 0.1f : 1f;
+
         PropertiesWindow propertiesWindow = Window.getInstance().getImGuiLayer().getPropertiesWindow();
         GameObject activeGameObject = propertiesWindow.getActiveGameObject();
         List<GameObject> activeGameObjects = propertiesWindow.getActiveGameObjects();
 
         if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_LEFT_CONTROL)
-            && KeyListener.keyBeginPress(GLFW.GLFW_KEY_D)
-            && activeGameObject != null
+                && KeyListener.keyBeginPress(GLFW.GLFW_KEY_D)
+                && activeGameObject != null
         ) {
             GameObject newObj = activeGameObject.copy();
             Window.getScene().addGameObjectToScene(newObj);
@@ -30,8 +36,8 @@ public class KeyControls extends Component {
                 newObj.getComponent(StateMachine.class).refreshTextures();
             }
         } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_LEFT_CONTROL)
-            && KeyListener.keyBeginPress(GLFW.GLFW_KEY_D)
-            && !activeGameObjects.isEmpty()) {
+                && KeyListener.keyBeginPress(GLFW.GLFW_KEY_D)
+                && !activeGameObjects.isEmpty()) {
             List<GameObject> gameObjects = new ArrayList<>(activeGameObjects);
             propertiesWindow.clearSelected();
             for (GameObject gameObject : gameObjects) {
@@ -47,6 +53,36 @@ public class KeyControls extends Component {
                 gameObject.destroy();
             }
             propertiesWindow.clearSelected();
+        } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_PAGE_DOWN) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject gameObject1 : activeGameObjects) {
+                gameObject1.transform.zIndex--;
+            }
+        } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_PAGE_UP) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject gameObject1 : activeGameObjects) {
+                gameObject1.transform.zIndex++;
+            }
+        } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_UP) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject gameObject1 : activeGameObjects) {
+                gameObject1.transform.position.y += Settings.GRID_HEIGHT * multiplier;
+            }
+        } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_LEFT) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject gameObject1 : activeGameObjects) {
+                gameObject1.transform.position.x -= Settings.GRID_WIDTH * multiplier;
+            }
+        } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_RIGHT) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject gameObject1 : activeGameObjects) {
+                gameObject1.transform.position.x += Settings.GRID_WIDTH * multiplier;
+            }
+        } else if (KeyListener.isKeyPressed(GLFW.GLFW_KEY_DOWN) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject gameObject1 : activeGameObjects) {
+                gameObject1.transform.position.y -= Settings.GRID_HEIGHT * multiplier;
+            }
         }
     }
 }
